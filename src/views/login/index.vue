0ba8af2be78b265327287119fd28392091e285b3
Mentu <2621082160@qq.com>
2023-11-22 20:08:06 +0800

    annotation(登录页): 修改登录页 默认登录数据

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -75,7 +75,7 @@ export default {
     return {
       loginForm: {
         username: 'admin',
-        password: '111111'
+        password: '123123'
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
